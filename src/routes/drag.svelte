13f62c7c402be6380fcf9f90a58e53a8e230eb1d
Mathias Picker <mattis123@live.no>
2022-05-10 11:26:04 +0200

    format

--- src/routes/drag.svelte
+++ src/routes/drag.svelte
@@ -3,48 +3,48 @@
 </script>
 
 <script lang="ts">
-    type TDraggableItem = {
-        id: number
-        name: string
-    }
+	type TDraggableItem = {
+		id: number;
+		name: string;
+	};
 
-    const draggableItems = [
-        {
-            id: 1,
-            name: 'foo',
-        },
-        {
-            id: 2,
-            name: 'bar',
-        },
-    ]
+	const draggableItems = [
+		{
+			id: 1,
+			name: 'foo'
+		},
+		{
+			id: 2,
+			name: 'bar'
+		}
+	];
 
-    let droppedItems: TDraggableItem[] = []
+	let droppedItems: TDraggableItem[] = [];
 
-    let currentItemBeingDragged: null | TDraggableItem = null
-    let draggingOverDropZone = false;
-    
-    function onDrop() {
-        if(currentItemBeingDragged) {
-            droppedItems = [...droppedItems, currentItemBeingDragged]
-        }
-    }
+	let currentItemBeingDragged: null | TDraggableItem = null;
+	let draggingOverDropZone = false;
 
-    function onDragStart(item: TDraggableItem) {
-        currentItemBeingDragged = item
-    }
+	function onDrop() {
+		if (currentItemBeingDragged) {
+			droppedItems = [...droppedItems, currentItemBeingDragged];
+		}
+	}
 
-    function onDragEnd() {
-        currentItemBeingDragged = null
-    }
+	function onDragStart(item: TDraggableItem) {
+		currentItemBeingDragged = item;
+	}
 
-    function onDragOver() {
-        draggingOverDropZone = true
-    }
+	function onDragEnd() {
+		currentItemBeingDragged = null;
+	}
 
-    function onDragLeave() {
-        draggingOverDropZone = false
-    }
+	function onDragOver() {
+		draggingOverDropZone = true;
+	}
+
+	function onDragLeave() {
+		draggingOverDropZone = false;
+	}
 </script>
 
 <svelte:head>
@@ -52,21 +52,32 @@
 </svelte:head>
 
 <section>
-    {#each draggableItems as item (item.id)}
-        <div class="draggable" draggable="true" on:dragstart={() => onDragStart(item)} on:dragend={onDragEnd}>
-            I am {item.name}
-        </div>
-    {/each}
+	{#each draggableItems as item (item.id)}
+		<div
+			class="draggable"
+			draggable="true"
+			on:dragstart={() => onDragStart(item)}
+			on:dragend={onDragEnd}
+		>
+			I am {item.name}
+		</div>
+	{/each}
 </section>
-<section class="drop-zone" class:active={draggingOverDropZone} on:dragover|preventDefault={onDragOver}  on:dragleave={onDragLeave} on:drop|stopPropagation={onDrop}>
-    Drop zone!
+<section
+	class="drop-zone"
+	class:active={draggingOverDropZone}
+	on:dragover|preventDefault={onDragOver}
+	on:dragleave={onDragLeave}
+	on:drop|stopPropagation={onDrop}
+>
+	Drop zone!
 </section>
 <section class="drop-log">
-    Dropped items log:
+	Dropped items log:
 
-    {#each droppedItems as item, i (i)}
-        <p>{item.name} was dropped</p>
-    {/each}
+	{#each droppedItems as item, i (i)}
+		<p>{item.name} was dropped</p>
+	{/each}
 </section>
 
 <style>
@@ -78,27 +89,27 @@
 		flex: 1;
 	}
 
-    .draggable {
-        cursor: grab;
-        padding: 2rem;
-        background-color: rgb(255, 255, 255);
-        border: 1px solid black;
-    }
+	.draggable {
+		cursor: grab;
+		padding: 2rem;
+		background-color: rgb(255, 255, 255);
+		border: 1px solid black;
+	}
 
-    .drop-zone {
-        margin-top: 2rem;
-        padding: 10rem;
-        border: 2px solid orange;
-    }
+	.drop-zone {
+		margin-top: 2rem;
+		padding: 10rem;
+		border: 2px solid orange;
+	}
 
-    .drop-zone.active {
-        background-color: aliceblue;
-    }
+	.drop-zone.active {
+		background-color: aliceblue;
+	}
 
-    .drop-log {
-        margin-top: 2rem;
-        font-weight: bold;
-    }
+	.drop-log {
+		margin-top: 2rem;
+		font-weight: bold;
+	}
 
 	h1 {
 		width: 100%;
